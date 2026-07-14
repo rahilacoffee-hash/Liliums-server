f20d2a4d6c2dd9f8705514c21803577e5e3a0b2f
fred <abrahamfred123@gmail.com>
2026-07-14 22:16:21 +0100

    Add

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 8080;
 // --------------------
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://liliums-glee.vercel.app",
+    ],
     credentials: true,
   })
 );
